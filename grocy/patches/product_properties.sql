47574d6d2562e281437ecf8a142087c954d916c1
BitBasherr <betalik@outlook.com>
2025-07-17 01:54:31 -0500

    fixes

---
+++ grocy/patches/product_properties.sql
@@ -0,0 +1,10 @@
+INSERT OR IGNORE INTO product_properties (id, name, description) VALUES 
+(1, 'Energy (kcal/100g)', 'Energy in kilocalories per 100 grams'),
+(2, 'Fat (g/100g)', 'Fat in grams per 100 grams'),
+(3, 'Saturated Fat (g/100g)', 'Saturated fat in grams per 100 grams'),
+(4, 'Carbohydrates (g/100g)', 'Carbohydrates in grams per 100 grams'),
+(5, 'Sugars (g/100g)', 'Sugars in grams per 100 grams'),
+(6, 'Fiber (g/100g)', 'Fiber in grams per 100 grams'),
+(7, 'Protein (g/100g)', 'Protein in grams per 100 grams'),
+(8, 'Salt (g/100g)', 'Salt in grams per 100 grams'),
+(9, 'Sodium (g/100g)', 'Sodium in grams per 100 grams');
